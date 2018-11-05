ceeebecb4788fe8554044acf46cd496facbce5b2
Ernesto PC <Ernesto PC@DESKTOP-8QCAJ0I>
2018-11-05 17:36:18 -0600

    packets Con, DAO, Model

---
+++ src/java/Controller/pruebaCon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Ernesto PC
+ */
+@Named(value = "pruebaCon")
+@Dependent
+public class pruebaCon {
+
+    /**
+     * Creates a new instance of pruebaCon
+     */
+    public pruebaCon() {
+    }
+    
+}
